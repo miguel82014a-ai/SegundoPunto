0da87c39843a3b33085e0c0d26aa9c2c927c9aec
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-16 12:02:38 -0500

    Cree un paquete Modelo con las clases Figura,Punto y Rectangulo

---
+++ src/main/java/com/mycompany/segundopunto/SegundoPunto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segundopunto;
+
+/**
+ *
+ * @author migue
+ */
+public class SegundoPunto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
