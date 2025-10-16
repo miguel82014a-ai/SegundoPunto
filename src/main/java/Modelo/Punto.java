fb118c113891200e638a52303e9828dd844d0e41
miguel82014a-ai <miguel82014a@gmail.com>
2025-10-16 14:32:16 -0500

    Añadi el codigo de la clase Punto

--- src/main/java/Modelo/Punto.java
+++ src/main/java/Modelo/Punto.java
@@ -9,5 +9,28 @@ package Modelo;
  * @author migue
  */
 public class Punto {
+    private double x;
+    private double y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
     
+      public void mover(double cambioX, double cambioY) {
+        x = x + cambioX;
+        y = y + cambioY;
+    }
+
+    public String mostrarCoordenadas() {
+        return "(" + x + ", " + y + ")";
+    }
 }
